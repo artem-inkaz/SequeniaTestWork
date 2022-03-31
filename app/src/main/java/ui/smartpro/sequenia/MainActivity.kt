cc8c80e82829af4e6a78d5baa048f6d0f3b84a8e
artem_inkaz <artem.inkaz@gmail.com>
2022-03-31 14:17:27 +0600

    Initial commit

---
+++ app/src/main/java/ui/smartpro/sequenia/MainActivity.kt
@@ -0,0 +1,11 @@
+package ui.smartpro.sequenia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
